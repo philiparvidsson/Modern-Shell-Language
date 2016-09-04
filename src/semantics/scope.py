f92b7ceb4d78c7f863d47accfcf67ae69a6f62c1
Philip Arvidsson <philip@philiparvidsson.com>
2016-09-04 21:53:06 +0200

    if-else-statements

--- src/semantics/scope.py
+++ src/semantics/scope.py
@@ -12,18 +12,7 @@ class Scope(object):
     def __init__(self, parent_scope=None):
         self.parent_scope = parent_scope
 
-        self.param_counter = 0
         self.variables = {}
-
-    def declare_parameter(self, name, type_):
-        self.param_counter += 1
-        var = Variable('~{}'.format(self.param_counter), type_)
-
-        print 'declared parameter {} of type {} (index={})'.format(name, type_, self.param_counter)
-
-        self.variables[name] = var
-
-        return var
 
     def declare_variable(self, name, type_):
         var = Variable(name, type_)
