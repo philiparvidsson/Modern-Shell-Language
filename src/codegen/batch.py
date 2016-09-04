127ab2cb05e45b4f6c9f62743303309bc59fd716
Philip Arvidsson <philip@philiparvidsson.com>
2016-09-04 14:06:16 +0200

    some refactoring

--- src/codegen/batch.py
+++ src/codegen/batch.py
@@ -2,46 +2,26 @@
 # IMPORTS
 #--------------------------------------------------
 
+from .codegenerator import code_emitter, CodeGenerator
+
 from parsing import syntax
 
 #--------------------------------------------------
 # CLASSES
 #--------------------------------------------------
 
-class Batch(object):
+class Batch(CodeGenerator):
     def __init__(self, ast):
-        self.construct_map = {
-            syntax.ADD        : self.__add,
-            syntax.ASSIGN     : self.__assign,
-            syntax.FUNC       : self.__func,
-            syntax.FUNC_CALL  : self.__func_call,
-            syntax.IDENTIFIER : self.__identifier,
-            syntax.INTEGER    : self.__integer,
-            syntax.PROGRAM    : self.__program,
-        }
+        super(Batch, self).__init__(ast)
 
-        self.ast = ast
-        self.code = ''
         self.value_stack = []
         self.tempvar_counter = 0
-
-    def emit(self, code):
-        self.code += code + '\n'
-
-    def generate_code(self, root=None):
-        if not root:
-            root = self.ast
-
-        code_func = self.construct_map[root.construct]
-
-        code_func(root)
-
-        return self.code
 
     def tempvar_name(self):
         self.tempvar_counter += 1
         return '_{}'.format(self.tempvar_counter)
 
+    @code_emitter(syntax.ADD)
     def __add(self, node):
         lhs = node.children[0]
         rhs = node.children[1]
@@ -53,6 +33,7 @@ class Batch(object):
         self.emit('set /a {}={}+{}'.format(temp, self.value_stack.pop(), self.value_stack.pop()))
         self.value_stack.append('!{}!'.format(temp))
 
+    @code_emitter(syntax.ASSIGN)
     def __assign(self, node):
         switches = []
 
@@ -67,21 +48,25 @@ class Batch(object):
         else:
             self.emit('set {}={}'.format(ident.data, self.value_stack.pop()))
 
-
+    @code_emitter(syntax.FUNC)
     def __func(self, node):
         self.emit(':{}'.format(node.data))
 
         self.emit('exit /b')
 
+    @code_emitter(syntax.FUNC_CALL)
     def __func_call(self, node):
         self.emit('call :{} args'.format(node.data))
 
+    @code_emitter(syntax.IDENTIFIER)
     def __identifier(self, node):
         self.value_stack.append('!{}!'.format(node.data))
 
+    @code_emitter(syntax.INTEGER)
     def __integer(self, node):
         self.value_stack.append(node.data)
 
+    @code_emitter(syntax.PROGRAM)
     def __program(self, node):
         self.emit('setlocal enabledelayedexpansion')
 
