42ee10f9de2863c0b7278f362c0edbb96d509476
Philip Arvidsson <philip@philiparvidsson.com>
2016-09-08 11:20:11 +0200

    more tests

--- src/tests/associativity.js
+++
@@ -1,9 +0,0 @@
-include('tests/test.js')
-
-test.name = "associativity"
-
-a = 2*3 + 4
-b = 4 + 3*2
-
-assert.equal(a, 10, 'multiplication gave incorrect result')
-assert.equal(b, 10, 'multiplication gave incorrect result')
