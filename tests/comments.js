e95b60b03b8193318aa91812604266ebce96818d
Philip Arvidsson <philip@philiparvidsson.com>
2016-09-10 10:33:49 +0200

    comment test

---
+++ tests/comments.js
@@ -0,0 +1,20 @@
+// This file doesn't do anything, it just contains some complex comment cases.
+a=/*...*/1
+/**////**/
+b/*=*/=/*2*/2//
+/*
+*//*
+*///
+
+/* /* */ //*/
+/**
+ * ///
+ *//**//*
+ *
+ * // */
+
+
+// Just to prevent warnings.
+a/*asd*/++
+b/*--*/++
+///*this comment reaches eof
