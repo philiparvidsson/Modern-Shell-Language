463720701d174fe323b63db67acdab5ba0390476
Philip Arvidsson <philip@philiparvidsson.com>
2016-09-10 15:58:19 +0200

    more testing and removed escaping of strings

--- tests/file.js
+++ tests/file.js
@@ -2,5 +2,9 @@ include('inc/testing.js')
 
 test.name = 'file operations test'
 
-s = file.read('file.txt')
+FILE_NAME = 'file.txt'
+
+assert.isTrue(file.exists(FILE_NAME), "file needed for test doesn't exist")
+
+s = file.read(FILE_NAME)
 assert.areEqual(s, 'hello world', 'file contents read incorrectly')
