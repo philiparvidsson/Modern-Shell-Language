65c508926661bbda9d2b0c730d8ecb93a1532725
Philip Arvidsson <philip@philiparvidsson.com>
2016-09-10 16:21:40 +0200

    more testing stuff

--- tests/file.js
+++ tests/file.js
@@ -5,6 +5,7 @@ test.name = 'file operations test'
 FILE_NAME = 'file.txt'
 
 assert.isTrue(file.exists(FILE_NAME), "file needed for test doesn't exist")
+assert.isFalse(file.exists('nonexistantfile'), "file should not exist")
 
 s = file.read(FILE_NAME)
 assert.areEqual(s, 'hello world', 'file contents read incorrectly')
