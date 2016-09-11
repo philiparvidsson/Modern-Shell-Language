aba4e6a2fd5959c3a58c74084703a9a193e62104
Philip Arvidsson <philip@philiparvidsson.com>
2016-09-11 16:59:41 +0200

    more tests

---
+++ tests/functions.js
@@ -0,0 +1,22 @@
+include('inc/testing.js')
+
+function a(x, y) {
+    assert.areEqual(x, 'hello', 'x should contain hello')
+    assert.areEqual(y, 'world', 'y should contain world')
+}
+
+function b(x, y) {
+    assert.areEqual(x, 'hello world', 'x should contain hello world')
+}
+
+function c() {
+    return 'hello world'
+}
+
+
+a('hello', 'world')
+b('hello world')
+
+assert.areEqual(c(), 'hello world', 'c() should return hello world')
+
+b(c())
