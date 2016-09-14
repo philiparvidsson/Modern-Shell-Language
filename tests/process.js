816d9029b34f8b94b487c9c972e7ec5db200ea9a
matteyas <matteyas@gmail.com>
2016-09-14 22:40:54 +0200

    test process.sleep()

--- tests/process.js
+++ tests/process.js
@@ -4,7 +4,7 @@ assert.areEqual(process.argv.length, 2, 'test should have received two args')
 assert.areEqual(process.argv[0], 'abc', 'first arg not received')
 assert.areEqual(process.argv[1], '123', 'second arg not received')
 
-// still FIXME: come up with a better test lol (add support for datetimes)
+// FIXME: come up with a better test lol (add support for datetimes)
 raw('set /a w1=!time:~6,2! * 100 + !time:~9,2!','bat')
 process.sleep(1)
 raw('set /a w2=!time:~6,2! * 100 + !time:~9,2!','bat')
