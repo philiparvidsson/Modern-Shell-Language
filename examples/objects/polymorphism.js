b2f97cb9643976ce64b5a0f77ed770c1fb348c46
Philip Arvidsson <philip@philiparvidsson.com>
2016-09-14 10:15:47 +0200

    more comments, closes #2

--- examples/objects/polymorphism.js
+++ examples/objects/polymorphism.js
@@ -1,8 +1,18 @@
+// This is a kind of constructor function for the base class. You can
+// think of it as the abstract super class initializer function.
 function animal(kind) {
+    // We don't have to use the variable name 'this', but it
+    // illustrates the intent of this function very well.
     this = []
 
+
+    // Here, we add a 'kind' property to the object,
     this.kind = kind
 
+
+    // This is a kind of instance method. We can reference any outer
+    // variable through closures so we could use 'this' inside it.
+    // Take a look at the instance methods in cow() and animal()
     this.say = function () {
         console.log('hello from base class')
     }
@@ -10,6 +20,7 @@ function animal(kind) {
     return this
 }
 
+// The cow constructor creates a cow object derived from the animal class.
 function cow(name) {
     this = animal('cow')
 
@@ -25,6 +36,7 @@ function cow(name) {
     return this
 }
 
+// The duck constructor creates a duck object derived from the animal class.
 function duck(name) {
     this = animal('duck')
 
@@ -37,8 +49,10 @@ function duck(name) {
     return this
 }
 
+// Let's create two derived instances.
 charlie = cow('charlie')
 anna = duck('anna')
 
+// And then invoke their methods.
 charlie.say('hello world')
 anna.say('good morning')
