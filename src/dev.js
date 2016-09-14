8fdbb0e676fb8da279eb71509258639324ac3129
Philip Arvidsson <philip@philiparvidsson.com>
2016-09-14 21:03:33 +0200

    typo

--- src/dev.js
+++ src/dev.js
@@ -1,20 +1,5 @@
-/*a = []
-
-a.push = function (w) {
-    a[1*a.length] = w
-    a.length = 1*a.length+1
+function a(x) {
+    console.log(x == 'hej san bos se')
 }
 
-
-a.push('abc')
-a.push('123')
-
-for (i = 0; i < a.length; i++) console.log(a[i])
-*/
-include ('../lib/array.js')
-
-a = Array()
-a.push('hej')
-a.push('bosse')
-
-while (a.length > 0) console.log(a.pop())
+a('hej san bos se')
