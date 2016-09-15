9f054030bb6447088e747dc6fdbf20342173e2c2
Philip Arvidsson <philip@philiparvidsson.com>
2016-09-15 09:35:12 +0200

    working on file class

--- src/dev.js
+++ src/dev.js
@@ -1,15 +1,4 @@
-include('../lib/array.js')
 include('../lib/console.js')
+include('../lib/file.js')
 
-a = Array([])
-a.push('hej')
-a.push('bosse')
-a.push('och')
-a.push('anita')
-a.insert('carl', 2)
-println(a.removeAt(0))
-println('pop')
-
-while (!a.isEmpty()) {
-    println(a.pop())
-}
+println(File.exists('deasdsv.js'))
