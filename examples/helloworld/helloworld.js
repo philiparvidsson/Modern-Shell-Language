94edf152ff2eba2a85bc95e0a4d41f1675b57ddc
Philip Arvidsson <philiparvidsson@users.noreply.github.com>
2016-09-15 23:30:24 +0200

    Update helloworld.js

--- examples/helloworld/helloworld.js
+++ examples/helloworld/helloworld.js
@@ -1,10 +1,12 @@
+include('stdio.js')
+
 // Simple hello world script.
 
 // Print 'hello world' to the console.
-console.log('hello world!')
+println('hello world!')
 
 // Ask the user to input what day it is...
-day = readline('what day is it?')
+day = input('what day is it?')
 
 // ...the print it back to the console.
-console.log("it's nice that it's", day)
+println("it's nice that it's", day)
