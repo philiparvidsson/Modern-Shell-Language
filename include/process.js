452226d5e4d16cea763913909290b9a43366dab2
Philip Arvidsson <philip@philiparvidsson.com>
2016-09-15 14:55:35 +0200

    closes #9

---
+++ include/process.js
@@ -0,0 +1,1 @@
+// TODO: move process.py buit-in here
---
+++ include/string.js
@@ -0,0 +1,26 @@
+String = function (s) {
+    this = []
+
+    this.value = s
+
+    this.indexOf = function (s) {
+        console.log('String.indexOf not implemented')
+        return this.value+s
+    }
+
+    this.length = function () {
+        console.log('String.length not implemented')
+        return 'very long'
+    }
+
+    this.substr = function () {
+        console.log('String.substr not implemented')
+        return 'very long'
+    }
+
+    return this
+}
+
+String.concat = function (a, b) {
+    return a.value+b.value
+}
