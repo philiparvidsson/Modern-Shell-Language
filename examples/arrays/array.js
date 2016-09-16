a2fc9057fa02b4fd2cdac11263518f66a980fb01
Philip Arvidsson <philip@philiparvidsson.com>
2016-09-16 08:57:51 +0200

    tweaks

--- examples/arrays/array.js
+++ examples/arrays/array.js
@@ -3,13 +3,13 @@ include('stdio.js')
 
 // This example uses the mutable Array class provided by the mshl standard library.
 
-a = Array()
+a = array.new()
 
 a.push('a')
 a.push('c')
 
 a.insert('b', 1)
 
-while (a.length() > 0) {
+while (a.length > 0) {
     println(a.pop())
 }
